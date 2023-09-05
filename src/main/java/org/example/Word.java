75b31235543af4f90e9e3f4cbefd5a0dc85893ad
csee22-076 <csee22200076@gmail.com>
2023-09-05 17:12:33 +0900

    Create getter and setter in Word Class

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -4,4 +4,28 @@ public class Word {
     private ArrayList<Byte> dif = new ArrayList<>();
     private ArrayList<String> newWord = new ArrayList<>();
     private ArrayList<String> meaning = new ArrayList<>();
+
+    public ArrayList<Byte> getDif() {
+        return dif;
+    }
+
+    public ArrayList<String> getNewWord() {
+        return newWord;
+    }
+
+    public ArrayList<String> getMeaning() {
+        return meaning;
+    }
+
+    public void setDif(ArrayList<Byte> dif) {
+        this.dif = dif;
+    }
+
+    public void setNewWord(ArrayList<String> newWord) {
+        this.newWord = newWord;
+    }
+
+    public void setMeaning(ArrayList<String> meaning) {
+        this.meaning = meaning;
+    }
 }
