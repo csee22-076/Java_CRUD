36f2ec2223a06e9e3cdf69fd95163a4a3ed962ec
csee22-076 <csee22200076@gmail.com>
2023-09-08 16:24:52 +0900

    Rename object name wm to WordMain

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        WordMan wm = new WordMan();
-        wm.run();
+        WordMan WordMain = new WordMan();
+        WordMain.run();
     }
 }
